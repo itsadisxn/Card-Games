ccab352a3741f4d07306a5e9dac982580c38bdd8
itsadisxn <55608486+itsadisxn@users.noreply.github.com>
2021-02-18 20:29:21 -0500

    edited javascript structure

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,41 @@
+/** 
+ * 1. We will start this project by building an IFFE function ( Immediately Invoked Function Expression ). 
+ * IFFE is a JavaScript function that runs as soon as it is defined.
+ * 
+ * 2. As can be seen below, the IFFE Expression has a semi-colon ";" before it. This has been added, in case the file is minified at any point of time and the preceding code does not have a semicolon
+*/
 ;(function(window){
-    // logic here
+    
+    class Game {
+        constructor() {
+            //Information Section
+            //Deck
+            //Discard Pile
+            //Rules
+        }
+    }
+
+    class Deck {
+        constructor() {
+            //Cards
+            //Shuffle 
+            //Stack
+        }
+    }
+
+    class Card {
+        constructor() {
+            //Value
+            //Suit
+            //Flip
+        }    
+    }
+
+    class DiscardPile {
+        constructor() {
+            //Holders
+            //Accept or Reject
+        }
+    }
 })(window);
+
