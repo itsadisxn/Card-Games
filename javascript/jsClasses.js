c31fef39c8978250958ea3da452137c488f71c44
itsadisxn <55608486+itsadisxn@users.noreply.github.com>
2021-02-18 20:13:17 -0500

    created basic javascript structure

---
+++ javascript/jsClasses.js
@@ -0,0 +1,19 @@
+class Game {
+    constructor() {
+      }
+}
+
+class Deck {
+    constructor() {
+    }
+}
+
+class Card {
+    constructor() {
+    }    
+}
+
+class DiscardPile {
+    constructor() {
+    }
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,3 @@
-;(function () {
+;(function(window){
     // logic here
-})();
+})(window);
